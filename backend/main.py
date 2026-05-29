7be0c811df88dc7f60efadee40d100fc240e5a31
ayushsingh1524 <singhayush9179@gmail.com>
2026-05-29 16:18:07 +0530

    fix(backend): allow all https origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_origin_regex="https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
